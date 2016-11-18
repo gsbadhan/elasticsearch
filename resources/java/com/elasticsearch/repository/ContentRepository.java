58073a2b3e916cb21078feb62261b63a92e33f25
gsbadhan <gurpreet.badhan.28@gmail.com>
2016-11-18 10:06:21 +0530

    new changes

--- resources/java/com/elasticsearch/repository/ContentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.elasticsearch.repository;
-
-import com.elasticsearch.model.Content;
-
-public interface ContentRepository extends ModelDrivenCRUDRepository<Content, Long> {
-}
--- resources/java/com/elasticsearch/repository/TagRepository.java
+++
@@ -1,6 +0,0 @@
-package com.elasticsearch.repository;
-
-import com.elasticsearch.model.Tag;
-
-public interface TagRepository extends ModelDrivenCRUDRepository<Tag, Long> {
-}
